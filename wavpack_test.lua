c1f3537790d5dffbc2a784e5900fa21cf94e98f1
daurnimator <quae@daurnimator.com>
2011-10-29 17:38:06 +1100

    * Fix output type

--- wavpack_test.lua
+++ wavpack_test.lua
@@ -1,5 +1,7 @@
 -- A test for luajit ffi based wavpack bindings
 
+local ioopen = io.open
+
 local general 				= require"general"
 local pretty 				= general.pretty_print
 local current_script_dir 	= general.current_script_dir
@@ -17,5 +19,15 @@ local wc = wavpack.openfile ( inputfile )
 local info = wc:getinfo ( )
 pretty ( info )
 
-dest = ffi.new ( "int32_t[?]" , info.num_samples * info.channels )
-wc:unpack ( dest , info.num_samples )
+local size = info.num_samples * info.channels
+local dest = ffi.new ( "int32_t[?]" , size )
+print ( "Decoded " , wc:unpack ( dest , info.num_samples ) )
+
+local out_buff = ffi.new ( "int16_t[?]" , size )
+for i=0,size-1 do
+	out_buff [ i ] = dest [ i ]
+end
+
+local out = assert ( ioopen ( 'samples.raw' , 'wb' ) )
+out:write ( ffi.string ( out_buff , ffi.sizeof ( out_buff ) ) )
+out:close ( )
